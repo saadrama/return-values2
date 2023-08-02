d64d6e49614989e340b6bf3a8ced01d74d643d33
Ramah Saad <ramahsaad20@gmail.com>
2023-08-02 23:08:07 +0300

    another commit on return

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    static int myMethod(int x,int y) {
+        return x+y;
+    }
+    public static void main(String[] args) {
+        int z= myMethod(7, 4);
+        System.out.println(z);
+    }
+}
